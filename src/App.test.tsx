5f1ff6fe9c7f9c62f5f3c12b7517d84610574df1
Angelika <szczepanczykangelika@gmail.com>
2022-11-18 17:46:05 +0100

    fix: test dependency

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { LandingPage } from "./pages/LandingPage";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test("renders landing page", () => {
+  render(<LandingPage />);
+  const linkElement = screen.getByText(/Landing Page/i);
   expect(linkElement).toBeInTheDocument();
 });
