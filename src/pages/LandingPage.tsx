d3fb79a63072cfce8db9dbc2c6f2748f5fa1a12c
Angelika <szczepanczykangelika@gmail.com>
2022-11-19 23:18:34 +0100

    update: responsive header styles

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -11,36 +11,38 @@ export const LandingPage = () => {
         </div>
         <img className="down-arrow" src={arrow} alt="arrow"></img>
       </div>
-      <div className="about">
-        <h1 className="section-title"> About</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.
-        </p>
-      </div>
-      <div className="explore">
-        <h1 className="section-title">Explore</h1>
-        <p>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-          ab illo inventore veritatis et quasi architecto beatae vitae dicta
-          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-          aspernatur aut odit aut fugit
-        </p>
-      </div>
-      <div className="sources">
-        <h1 className="section-title">Sources</h1>
-        <p>
-          Nor again is there anyone who loves or pursues or desires to obtain
-          pain of itself, because it is pain, but because occasionally
-          circumstances occur in which toil and pain can procure him some great
-          pleasure. To take a trivial example, which of us ever undertakes
-          laborious physical exercise
-        </p>
+      <div className="lp-content">
+        <section>
+          <h1 className="section-title"> About</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </p>
+        </section>
+        <section>
+          <h1 className="section-title">Explore</h1>
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+            aspernatur aut odit aut fugit
+          </p>
+        </section>
+        <section>
+          <h1 className="section-title">Sources</h1>
+          <p>
+            Nor again is there anyone who loves or pursues or desires to obtain
+            pain of itself, because it is pain, but because occasionally
+            circumstances occur in which toil and pain can procure him some
+            great pleasure. To take a trivial example, which of us ever
+            undertakes laborious physical exercise
+          </p>
+        </section>
       </div>
     </div>
   );
