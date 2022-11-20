88101fc91e1b44da13cac2c44f48968ff8f565e3
Angelika <szczepanczykangelika@gmail.com>
2022-11-20 20:26:51 +0100

    update: sources section - style + text

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,5 @@
 import photoA from "../assets/images/perfume-bottle.jpg";
+import photoB from "../assets/images/perfume-bottle-2.jpg";
 const arrow: string = require("../assets/icons/down.svg").default;
 
 export const LandingPage = () => {
@@ -17,17 +18,17 @@ export const LandingPage = () => {
           <div className="section-containter">
             <h1 className="section-title"> ABOUT</h1>
             <p>
-              <a href="https://www.fragrantica.com/"> Fragrantica </a> is an
-              online encyclopedia of perfumes. It is created by true perfume
-              lovers for all enthusiasts of exploring the fragrant world. The
-              portal allows you to explore the portfolios of many brands in the
-              perfume industry. Each fragrance contains a list of the
-              composition's ingredients - divided by fragrance notes. Perfumes
-              are also classified by olfactor groups. Therefore allows users to
-              expand their knowledge and compare products with each other.
+              Fragrantica is an online encyclopedia of perfumes. It is created
+              by true perfume lovers for all enthusiasts of exploring the
+              fragrant world. The portal allows you to explore the portfolios of
+              many brands in the perfume industry. Each fragrance contains a
+              list of the composition's ingredients - divided by fragrance
+              notes. Perfumes are also classified by olfactor groups. Therefore
+              allows users to expand their knowledge and compare products with
+              each other.
             </p>
           </div>
-          <img src={photoA}></img>
+          <img src={photoA} alt="perfume bottle"></img>
         </section>
         <section className="explore-section">
           <div className="section-containter">
@@ -42,15 +43,44 @@ export const LandingPage = () => {
           </div>
         </section>
         <section className="sources-section">
+          <img src={photoB} alt="perfume bottle"></img>
           <div className="section-containter">
             <h1 className="section-title">SOURCES</h1>
             <p>
-              Nor again is there anyone who loves or pursues or desires to
-              obtain pain of itself, because it is pain, but because
-              occasionally circumstances occur in which toil and pain can
-              procure him some great pleasure. To take a trivial example, which
-              of us ever undertakes laborious physical exercise
+              The following list contains the sources of the content used in
+              this project. If you click on an item, you will be redirected.
             </p>
+            <ul className="list-of-sources">
+              <li>
+                <a href="https://www.fragrantica.com/">
+                  Information about perfumes <span>Fragrantica</span>
+                </a>
+              </li>
+              <li className="divider"> </li>
+              <li>
+                <a href="https://burst.shopify.com/photos/dramatic-black-glass-perfume?q=perfume">
+                  Header image <span>Sarah Pflug</span>
+                </a>
+              </li>
+              <li className="divider"> </li>
+              <li>
+                <a href="https://www.pexels.com/pl-pl/zdjecie/martwa-natura-pojemnik-perfumy-szklana-butelka-10688062/">
+                  Black perfume bottle image <span>Abdulrhman Alkady</span>
+                </a>
+              </li>
+              <li className="divider"> </li>
+              <li>
+                <a href="https://www.pexels.com/pl-pl/zdjecie/platforma-martwa-natura-jedwabisty-fotografia-produktowa-12402363/">
+                  Two bottles of perfume image <span>Marek Mucha</span>
+                </a>
+              </li>
+              <li className="divider"> </li>
+              <li>
+                <a href="https://www.svgrepo.com/">
+                  Icons <span>SVG REPO</span>
+                </a>
+              </li>
+            </ul>
           </div>
         </section>
       </div>
