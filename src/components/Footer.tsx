1c29ab8e8628f884873e1e5acae6a488b7457352
Angelika <szczepanczykangelika@gmail.com>
2022-11-29 17:32:49 +0100

    add: footer style and content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,12 @@
+const github: string = require("../assets/icons/github.svg").default;
+
 export const Footer = () => {
   return (
     <footer className="footer">
-      <a href="https://github.com/a-szczepan/fragrantica-clone">Source code</a>
+      <span>Source code available on GitHub </span>
+      <a href="https://github.com/a-szczepan/fragrantica-clone">
+        <img src={github} alt="github logo"></img>
+      </a>
     </footer>
   );
 };
