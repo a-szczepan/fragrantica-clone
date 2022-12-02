4c9b4c8937860cb935d9f9cf2900fd86aa8e3f5d
Angelika <szczepanczykangelika@gmail.com>
2022-12-02 12:42:14 +0100

    add: filtering modal + basic style

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,3 +1,23 @@
+import { useEffect } from "react";
+import { FilteringModal } from "../components/FilteringModal";
+import { useScrollPrevention } from "../hooks/useScrollPrevention";
+
 export const SearchPage = () => {
-  return <h1>Hello Search Page</h1>;
+  const [scrollPrevention, setScrollPrevention] = useScrollPrevention(false);
+
+  useEffect(() => {
+    console.log(scrollPrevention);
+  }, [scrollPrevention]);
+
+  return (
+    <div className="searchPage">
+      <button onClick={() => setScrollPrevention(true)}>Open Filters</button>
+      {scrollPrevention ? (
+        <FilteringModal
+          open={scrollPrevention}
+          onClose={() => setScrollPrevention(false)}
+        />
+      ) : null}
+    </div>
+  );
 };
