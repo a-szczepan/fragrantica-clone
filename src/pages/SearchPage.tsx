35673cb4e8bb1882006c9c41510e5e0bd4d4031f
Angelika <szczepanczykangelika@gmail.com>
2022-12-06 20:09:52 +0100

    add: filter button

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,12 +1,15 @@
 import { FilteringModal } from "../components/FilteringModal";
+import { Filters } from "../components/Filters";
 import { useScrollPrevention } from "../hooks/useScrollPrevention";
-
+const filter: string = require("../assets/icons/filter.svg").default;
 export const SearchPage = () => {
   const [scrollPrevention, setScrollPrevention] = useScrollPrevention(false);
 
   return (
     <div className="searchPage">
-      <button onClick={() => setScrollPrevention(true)}>Open Filters</button>
+      <button onClick={() => setScrollPrevention(true)}>
+        <img src={filter} alt="filter"></img>
+      </button>
       {scrollPrevention ? (
         <FilteringModal
           open={scrollPrevention}
